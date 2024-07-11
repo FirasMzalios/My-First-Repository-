4d528090d1800351d6b7ecb0145bbb82e333dc73
FirasMzalios <firasmzali.fst@gmail.com>
2024-07-11 02:44:05 +0100

    Create Different print commands.py

---
+++ Different print commands.py
@@ -0,0 +1,36 @@
+#so this is the most simple print command 
+print("\n")#this is like we tapped in Enter 
+
+#-------
+
+print("i'm the danger Tylor")
+#or 
+print('i"m the danger tylor')
+#the difference is between '' and "" but the same result 
+
+#okay pathetic this is too simple 
+
+#-------- 
+
+#multiline print 
+print("""woow this sh*t is getting crazy,
+       am I tripping or somthn,
+       yooo wher am III,
+      
+
+      why is the dround moving like this
+      
+      and that's everyone ois when you fall for the lsd :)""")
+
+#you need to add """ in each side
+
+#but what if we use this 
+print("hey guys i'm high right, \n now and decided to learn python \n and i'm teaching you through my repository so you can learn from it when you get high i think... \n anyways")
+
+#and there is this 
+
+print("i'm just wasting too much time in just" + " learning the FUCKING print command in Fucking python " +"\n i'm going to sleep")
+
+#last thing ... do you wanna multiply your phrase ?
+
+print(" eat sh*t sleep repeat" * 365 )
