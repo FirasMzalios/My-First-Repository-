c103b31cda3bacbb1eff1b2a3b4123f020f72470
FirasMzalios <firasmzali.fst@gmail.com>
2024-07-11 03:02:18 +0100

    Create Variables !.py

---
+++ Variables !.py
@@ -0,0 +1,10 @@
+#so before i get sleep let me explain to you how variables works: X is a variable that we can store in it a string, number, integer or even a mathematic formulat...
+
+X= 98
+
+print(X)
+
+A=X/98
+
+print(A)
+
